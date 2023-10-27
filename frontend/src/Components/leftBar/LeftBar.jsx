8fb4ec7f34343a9f79de75bf0dfc94d1fc0b7094
Shashank Tiwari <shashanktiwari1312@gmail.com>
2023-10-27 22:33:30 +0530

    leftbar component

--- frontend/src/Components/leftBar/LeftBar.jsx
+++ frontend/src/Components/leftBar/LeftBar.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import styles from './leftBar.module.css';
 
 function LeftBar() {
   return (
-    <div>LeftBar</div>
+    <>
+    <div className={styles.left}>
+      <h1>Quizzie</h1>
+      <span>
+        <p>Dashboard</p>
+        <p>Analytics</p>
+        <p>Create Quiz</p>
+      </span>
+      <span>
+        <hr />
+        <h3>Logout</h3>
+      </span>
+    </div>
+    </>
   )
 }
 
