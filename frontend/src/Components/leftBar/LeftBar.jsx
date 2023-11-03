105ae2cc5b7529a59900b90afd8d9700f2793379
Shashank Tiwari <shashanktiwari1312@gmail.com>
2023-11-03 22:58:36 +0530

    completed the left component

--- frontend/src/Components/leftBar/LeftBar.jsx
+++ frontend/src/Components/leftBar/LeftBar.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import styles from './leftBar.module.css';
+  import React from 'react';
+  import styles from './leftBar.module.css';
+  
 
-function LeftBar() {
-  return (
-    <>
-    <div className={styles.left}>
-      <h1>Quizzie</h1>
-      <span>
-        <p>Dashboard</p>
-        <p>Analytics</p>
-        <p>Create Quiz</p>
-      </span>
-      <span>
-        <hr />
-        <h3>Logout</h3>
-      </span>
-    </div>
-    </>
-  )
-}
+  function LeftBar() {
+    return (
+      <>
+      <div className={styles.left}>
+        <h1>Quizzie</h1>
+        <span className={styles.para}>
+          <button>Dashboard</button>
+          <button>Analytics</button>
+          <button>Create Quiz</button>
+        </span>
+        <span className={styles.bottom}>
+          <hr />
+          <h3>Logout</h3>
+        </span>
+      </div>
+      </>
+    )
+  }
 
-export default LeftBar;
+  export default LeftBar;
--- frontend/src/Pages/dashboard/Dashboard.jsx
+++ frontend/src/Pages/dashboard/Dashboard.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import LeftBar from '../../Components/leftBar/LeftBar';
+import styles from './dashboard.module.css';
+
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <>
+    <LeftBar />
+    <div className={styles.main}>
+      <div className={styles.head}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={styles.bottom}>
+        <h1>Trending Quizes</h1>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    </>
   )
 }
 
