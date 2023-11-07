b55b77ce2e2d77a38027cc3546445b54ca3317d4
Shashank Tiwari <shashanktiwari1312@gmail.com>
2023-11-07 07:33:44 +0530

    achieved the dashboard layout

--- frontend/src/Pages/dashboard/Dashboard.jsx
+++ frontend/src/Pages/dashboard/Dashboard.jsx
@@ -1,66 +1,113 @@
-import React from 'react';
-import LeftBar from '../../Components/leftBar/LeftBar';
-import styles from './dashboard.module.css';
-import {BsEye} from 'react-icons/bs';
-
+import React from "react";
+import LeftBar from "../../Components/leftBar/LeftBar";
+import styles from "./dashboard.module.css";
+import { BsEye } from "react-icons/bs";
 
 function Dashboard() {
   return (
     <>
-    <LeftBar />
-    <div className={styles.main}>
-      <div className={styles.head}>
-        <span className={styles.wrapper}>
-          <h1>12</h1>
-          <p><span>Quiz </span>Created</p>
+      <LeftBar />
+      <div className={styles.main}>
+        <div className={styles.head}>
+          <span className={styles.wrapper}>
+            <h1>12</h1>
+            <p>
+              <span>Quiz </span>Created
+            </p>
+          </span>
+          <span className={styles.wrapper}>
+            <h1>110</h1>
+            <p>
+              <span>Question</span> Created
+            </p>
+          </span>
+          <span className={styles.wrapper}>
+            <h1>989</h1>
+            <p>
+              <span>Total</span> Impression
+            </p>
+          </span>
+        </div>
+        <span className={styles.mid}>
+          <h1>Trending Quizes</h1>
         </span>
-        <span className={styles.wrapper}>
-          <h1>110</h1>
-          <p><span>Question</span>  Created </p>
-        </span>
-        <span className={styles.wrapper}>
-          <h1>989</h1>
-          <p><span>Total</span> Impression </p>
-        </span>
+        <div className={styles.bottom}>
+          <span>
+            <h2>Quiz 1</h2>
+            <p className={styles.num}>
+              667
+              <BsEye className={styles.BsEye} />
+            </p>
+            <p>Created on: 11 Nov, 2023</p>
+          </span>
+          <span>
+            <h2>Quiz 1</h2>
+            <p className={styles.num}>
+              667
+              <BsEye className={styles.BsEye} />
+            </p>
+            <p>Created on: 11 Nov, 2023</p>
+          </span>
+          <span>
+            <h2>Quiz 1</h2>
+            <p className={styles.num}>
+              667
+              <BsEye className={styles.BsEye} />
+            </p>
+            <p>Created on: 11 Nov, 2023</p>
+          </span>
+          <span>
+            <h2>Quiz 1</h2>
+            <p className={styles.num}>
+              667
+              <BsEye className={styles.BsEye} />
+            </p>
+            <p>Created on: 11 Nov, 2023</p>
+          </span>
+          <span>
+            <h2>Quiz 1</h2>
+            <p className={styles.num}>
+              667
+              <BsEye className={styles.BsEye} />
+            </p>
+            <p>Created on: 11 Nov, 2023</p>
+          </span>
+          <span>
+            <h2>Quiz 1</h2>
+            <p className={styles.num}>
+              667
+              <BsEye className={styles.BsEye} />
+            </p>
+            <p>Created on: 11 Nov, 2023</p>
+          </span>
+          <span>
+            <h2>Quiz 1</h2>
+            <p className={styles.num}>
+              667
+              <BsEye className={styles.BsEye} />
+            </p>
+            <p>Created on: 11 Nov, 2023</p>
+          </span>
+          <span>
+            <h2>Quiz 1</h2>
+            <p className={styles.num}>
+              667
+              <BsEye className={styles.BsEye} />
+            </p>
+            <p>Created on: 11 Nov, 2023</p>
+          </span>
+          <span>
+            <h2>Quiz 1</h2>
+            <p className={styles.num}>
+              667
+              <BsEye className={styles.BsEye} />
+            </p>
+            <p>Created on: 11 Nov, 2023</p>
+          </span>
+        </div>
       </div>
-      <span className={styles.mid}>
-        <h1>Trending Quizes</h1>
-      </span>
-      <div className={styles.bottom}>
-        <span>
-          <h2>Quiz 1</h2>
-          <p className={styles.num}>667<BsEye/></p>
-          <p>Created on: 11 Nov, 2023</p>
-        </span>
-        <span>
-          <h2>Quiz 1</h2>
-          <p className={styles.num}>667<BsEye/></p>
-          <p>Created on: 11 Nov, 2023</p>
-        </span>
-        <span>
-          <h2>Quiz 1</h2>
-          <p className={styles.num}>667<BsEye/></p>
-          <p>Created on: 11 Nov, 2023</p>
-        </span>
-        <span>
-          <h2>Quiz 1</h2>
-          <p className={styles.num}>667<BsEye/></p>
-          <p>Created on: 11 Nov, 2023</p>
-        </span>
-        <span>
-          <h2>Quiz 1</h2>
-          <p className={styles.num}>667<BsEye/></p>
-          <p>Created on: 11 Nov, 2023</p>
-        </span>
-        <span>
-          <h2>Quiz 1</h2>
-          <p className={styles.num}>667<BsEye/></p>
-          <p>Created on: 11 Nov, 2023</p>
-        </span>
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
 export default Dashboard;
